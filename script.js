ac17b74a3e975899e87f2993b8b8e6811d78ca0e
NJENGA-D <njengawambui466@gmail.com>
2026-02-20 09:56:20 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,28 @@
+// MOBILE MENU
+const menuBtn = document.getElementById("menuBtn");
+const navLinks = document.getElementById("navLinks");
+
+menuBtn.addEventListener("click", () => {
+  navLinks.style.display =
+    navLinks.style.display === "flex" ? "none" : "flex";
+});
+
+// HERO BUTTON
+document.getElementById("heroBtn").addEventListener("click", () => {
+  document.getElementById("about").scrollIntoView({
+    behavior: "smooth"
+  });
+});
+
+// CONTACT FORM (FRONTEND DEMO)
+const form = document.getElementById("contactForm");
+const message = document.getElementById("formMessage");
+
+
+  form.addEventListener("submit", function (e) {
+    e.preventDefault();
+    message.textContent = "WELCOM!";
+    message.style.color = "RED";
+    
+});
+
